de3120d0fdac326360829d9e9ee3addd6fedb013
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-09 20:27:01 +0000

    Created header React element

---
+++ public/app.js
@@ -0,0 +1,14 @@
+const title = 'My First React Element!'
+const desc = 'I just learned how to create a React node and render it into the DOM.'
+
+const header = (
+  <header>
+    <h1>{ title }</h1>
+    <p>{ desc }</p>
+  </header>
+)
+
+ReactDOM.render(
+  header,
+  document.getElementById('root')
+)
